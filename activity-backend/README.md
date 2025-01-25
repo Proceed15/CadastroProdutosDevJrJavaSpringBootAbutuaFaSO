13a35ab4e670c3dd8b4712c94d3d6a6adcd5e2b7
José Lisboa Filho <jlisboa600@gmail.com>
2025-01-25 00:38:41 -0300

    Raiz Criada

---
+++ activity-backend/README.md
@@ -0,0 +1,2 @@
+# CadastroProdutosDevJrJavaSpringBootAbutuaFaSO
+Bootcamp realizado com o objetivo de criar um cadastro de produtos como um desenvolvedor Junior
---
+++ activity-backend/src/main/java/com/jrlsf/activity_backend/resources/HelloController.java
@@ -0,0 +1,12 @@
+package com.jrlsf.activity_backend.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public String getHello(){
+        return "Hello Spring Boot + World!";
+    }
+}
